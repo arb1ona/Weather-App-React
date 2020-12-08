e43440e1f1e08c7f35236f83f11cc35b290c92b5
arb1ona <arbiona.shahu@gmail.com>
2020-12-08 23:02:39 +0100

    Text Styled

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ const api = {
 
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return`${day} ${date} ${month} ${year}`
+
+  }
+
+
   return (
     <div className="app">
       <main>
@@ -16,6 +31,14 @@ function App() {
             placeholder="Search..."
             className="search-bar"
           />
+        </div>
+        <div className="location-box" >
+          <div className="location">Tirana, Al</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°C</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
